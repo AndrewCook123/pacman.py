c802d1d82c95c31dc91cbc772457565ca19aab59
AndrewCook123 <148015806+AndrewCook123@users.noreply.github.com>
2023-10-24 17:34:30 -0400

    Add files via upload

---
+++ player.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 19 08:29:20 2023
+
+@author: Aceco
+"""
+
+import pygame
+
+class Player(object):
+    def __init__(self):
+        self.WIDTH = 900
+        self.HEIGHT = 950
+    
+    def move_player(self, play_x, play_y, turns_allowed, direction, player_speed):
+        # r, l, u, d
+        if direction == 0 and turns_allowed[0]:
+            play_x += player_speed
+        elif direction == 1 and turns_allowed[1]:
+            play_x -= player_speed
+        if direction == 2 and turns_allowed[2]:
+            play_y -= player_speed
+        elif direction == 3 and turns_allowed[3]:
+            play_y += player_speed
+        return play_x, play_y
+    
+    def draw_player(self, direction, player_x, player_y, counter, screen, player_images):
+        # 0-RIGHT, 1-LEFT, 2-UP, 3-DOWN
+        if direction == 0:
+            screen.blit(player_images[counter // 5], (player_x, player_y))
+        elif direction == 1:
+            screen.blit(pygame.transform.flip(player_images[counter // 5], True, False), (player_x, player_y))
+        elif direction == 2:
+            screen.blit(pygame.transform.rotate(player_images[counter // 5], 90), (player_x, player_y))
+        elif direction == 3:
+            screen.blit(pygame.transform.rotate(player_images[counter // 5], 270), (player_x, player_y))
+                
+    def check_position(self, centerx, centery, direction, level):
+        turns = [False, False, False, False]
+        num1 = (self.HEIGHT - 50) // 32
+        num2 = (self.WIDTH // 30)
+        num3 = 15
+        # check collisions based on center x and center y of player +/- fudge number
+        if centerx // 30 < 29:
+            if direction == 0:
+                if level[centery // num1][(centerx - num3) // num2] < 3:
+                    turns[1] = True
+            if direction == 1:
+                if level[centery // num1][(centerx + num3) // num2] < 3:
+                    turns[0] = True
+            if direction == 2:
+                if level[(centery + num3) // num1][centerx // num2] < 3:
+                    turns[3] = True
+            if direction == 3:
+                if level[(centery - num3) // num1][centerx // num2] < 3:
+                    turns[2] = True
+
+            if direction == 2 or direction == 3:
+                if 12 <= centerx % num2 <= 18:
+                    if level[(centery + num3) // num1][centerx // num2] < 3:
+                        turns[3] = True
+                    if level[(centery - num3) // num1][centerx // num2] < 3:
+                        turns[2] = True
+                if 12 <= centery % num1 <= 18:
+                    if level[centery // num1][(centerx - num2) // num2] < 3:
+                        turns[1] = True
+                    if level[centery // num1][(centerx + num2) // num2] < 3:
+                        turns[0] = True
+            if direction == 0 or direction == 1:
+                if 12 <= centerx % num2 <= 18:
+                    if level[(centery + num1) // num1][centerx // num2] < 3:
+                            turns[3] = True
+                    if level[(centery - num1) // num1][centerx // num2] < 3:
+                        turns[2] = True
+                if 12 <= centery % num1 <= 18:
+                    if level[centery // num1][(centerx - num3) // num2] < 3:
+                        turns[1] = True
+                    if level[centery // num1][(centerx + num3) // num2] < 3:
+                        turns[0] = True
+        else:
+            turns[0] = True
+            turns[1] = True
+
+        return turns
