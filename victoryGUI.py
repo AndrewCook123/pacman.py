53a2cfa3d4b14d06458dcebbcdc877aaa79e301c
cdragun53 <143451754+cdragun53@users.noreply.github.com>
2023-11-01 17:15:35 -0400

    Update victoryGUI.py

--- victoryGUI.py
+++ victoryGUI.py
@@ -1,20 +1,26 @@
 import pygame
+from pauseGUI import pause
+
 
 class VictoryGUI(object):
-    def victory(screen,font):
+    def __init__(self, screen,font):
+        self.clicked=False
+        self.screen=screen
+        self.font=font
+    def victory(self):
         pos = pygame.mouse.get_pos()
-        clicked = False
-        pygame.draw.rect(screen, 'white', [50, 200, 800, 300],0, 10)
-        pygame.draw.rect(screen, 'dark gray', [70, 220, 760, 260], 0, 10)
-        top=pygame.draw.rect(screen, 'red', [300, 250, 200, 200], 0, 10)
-        action = False
+        pygame.draw.rect(self.screen, 'white', [50, 200, 800, 300],0, 10)
+        pygame.draw.rect(self.screen, 'dark gray', [70, 220, 760, 260], 0, 10)
+        top=pygame.draw.rect(self.screen, 'red', [300, 250, 200, 200], 0, 10)
         if top.collidepoint(pos):
-            if pygame.mouse.get_pressed()[0] == 1 and clicked == False:
-                clicked = True
-                action = True
+            if pygame.mouse.get_pressed()[0] == 1:
+                self.clicked = True
                 print("Clicked")
+        if self.clicked:
+            new=pause(self.screen)
+            new.PauseMenu()
                 
     
         if pygame.mouse.get_pressed()[0] == 0:
-            clicked = False
-        return action
+            pass
+        return self.clicked
