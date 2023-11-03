a14e163b2aa7719f4135fd65b741fbbdfac16626
cdragun53 <143451754+cdragun53@users.noreply.github.com>
2023-11-02 20:49:15 -0400

    Update GameOverGUI.py

--- GameOverGUI.py
+++ GameOverGUI.py
@@ -5,13 +5,16 @@ class gameOver(object):
         self.font=font
         self.clicked_over=False
         self.status=status
+        self.Mainfont=pygame.font.Font('freesansbold.ttf', 40)
     def Gover(self):
         pos = pygame.mouse.get_pos()
         pygame.draw.rect(self.screen, 'white', [50, 200, 800, 300],0, 10)
         pygame.draw.rect(self.screen, 'dark gray', [70, 220, 760, 260], 0, 10)
-        gameoverbox=pygame.draw.rect(self.screen, 'Blue', [200, 400, 150, 50], 0, 10)
-        gameover_text = self.font.render('Game over! Space bar to restart!', True, 'red')
-        self.screen.blit(gameover_text, (100, 300))
+        gameoverbox=pygame.draw.rect(self.screen, 'Blue', [350, 400, 350, 50], 0, 10)
+        gameover_text = self.font.render('Game over! Push to restart', True, 'red')
+        self.screen.blit(gameover_text, (380, 420))
+        gameoverMenu_text = self.Mainfont.render('Game over Menu', True, 'red')
+        self.screen.blit(gameoverMenu_text, (350, 250))
         if gameoverbox.collidepoint(pos):
             if pygame.mouse.get_pressed()[0] == 1:
                 self.status = True
