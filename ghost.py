481b5e0f2cd10597ab00e3ea3cd30a1d243ab876
AndrewCook123 <148015806+AndrewCook123@users.noreply.github.com>
2023-11-21 17:54:24 -0500

    Update ghost.py

--- ghost.py
+++ ghost.py
@@ -1,68 +1,12 @@
-from board import boards
 import pygame
-import copy
-import math
 
-WIDTH = 900
-HEIGHT = 950
-screen = pygame.display.set_mode([WIDTH, HEIGHT])
-timer = pygame.time.Clock()
-fps = 60
-level = copy.deepcopy(boards)
-color = 'blue'
-PI = math.pi
-player_images = []
-for i in range(1, 5):
-    player_images.append(pygame.transform.scale(pygame.image.load(f'assets/player_images/{i}.png'), (45, 45)))
-blinky_img = pygame.transform.scale(pygame.image.load(f'assets/ghost_images/red.png'), (45, 45))
-pinky_img = pygame.transform.scale(pygame.image.load(f'assets/ghost_images/pink.png'), (45, 45))
-inky_img = pygame.transform.scale(pygame.image.load(f'assets/ghost_images/blue.png'), (45, 45))
-clyde_img = pygame.transform.scale(pygame.image.load(f'assets/ghost_images/orange.png'), (45, 45))
-spooked_img = pygame.transform.scale(pygame.image.load(f'assets/ghost_images/powerup.png'), (45, 45))
-dead_img = pygame.transform.scale(pygame.image.load(f'assets/ghost_images/dead.png'), (45, 45))
-player_x = 450
-player_y = 663
-direction = 0
-blinky_x = 56
-blinky_y = 58
-blinky_direction = 0
-inky_x = 440
-inky_y = 388
-inky_direction = 2
-pinky_x = 440
-pinky_y = 438
-pinky_direction = 2
-clyde_x = 440
-clyde_y = 438
-clyde_direction = 2
-counter = 0
-flicker = False
-# R, L, U, D
-turns_allowed = [False, False, False, False]
-direction_command = 0
-player_speed = 2
-score = 0
-powerup = False
-power_counter = 0
-eaten_ghost = [False, False, False, False]
-targets = [(player_x, player_y), (player_x, player_y), (player_x, player_y), (player_x, player_y)]
-blinky_dead = False
-inky_dead = False
-clyde_dead = False
-pinky_dead = False
-blinky_box = False
-inky_box = False
-clyde_box = False
-pinky_box = False
-moving = False
-ghost_speeds = [2, 2, 2, 2]
-startup_counter = 0
-lives = 3
-game_over = False
-game_won = False
+
+
 
 class Ghost(object):
-    def __init__(self, x_coord, y_coord, target, speed, img, direct, dead, box, powerup, id):
+    def __init__(self, x_coord, y_coord, target, speed, img, direct, dead, box, powerup, id, screen, eaten_ghost, level):
+        self.WIDTH = 900
+        self.HEIGHT = 950
         self.x_pos = x_coord
         self.y_pos = y_coord
         self.powerup = powerup
@@ -77,10 +21,17 @@ class Ghost(object):
         self.dead = dead
         self.in_box = box
         self.id = id
-        self.turns, self.in_box = self.check_collisions()
-        self.rect = self.draw()
+        self.level = level
+        self.screen = screen
+        self.eaten_ghost = eaten_ghost
+        self.spooked_img = pygame.transform.scale(pygame.image.load(f'assets/ghost_images/powerup.png'), (45, 45))
+        self.dead_img = pygame.transform.scale(pygame.image.load(f'assets/ghost_images/dead.png'), (45, 45))
+        self.turns, self.in_box = self.check_collisions(self.level)
+        self.rect = self.draw(self.screen, self.eaten_ghost, self.spooked_img, self.dead_img)
+        
+        
 
-    def draw(self):
+    def draw(self,screen, eaten_ghost, spooked_img, dead_img):
         if (not self.powerup and not self.dead) or (eaten_ghost[self.id] and self.powerup and not self.dead):
             screen.blit(self.img, (self.x_pos, self.y_pos))
         elif self.powerup and not self.dead and not eaten_ghost[self.id]:
@@ -90,14 +41,14 @@ class Ghost(object):
         ghost_rect = pygame.rect.Rect((self.center_x - 18, self.center_y - 18), (36, 36))
         return ghost_rect
 
-    def check_collisions(self):
+    def check_collisions(self, level):
         # R, L, U, D
-        num1 = ((HEIGHT - 50) // 32)
-        num2 = (WIDTH // 30)
+        num1 = ((self.HEIGHT - 50) // 32)
+        num2 = (self.WIDTH // 30)
         num3 = 15
         self.turns = [False, False, False, False]
         if 0 < self.center_x // 30 < 29:
-            if level[(self.center_y - num3) // num1][self.center_x // num2] == 9:
+            if self.level[(self.center_y - num3) // num1][self.center_x // num2] == 9:
                 self.turns[2] = True
             if level[self.center_y // num1][(self.center_x - num3) // num2] < 3 \
                     or (level[self.center_y // num1][(self.center_x - num3) // num2] == 9 and (
