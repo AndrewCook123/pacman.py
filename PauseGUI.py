ddc740f2a18e972105a01cc61ce8956e6854fd1b
AndrewCook123 <148015806+AndrewCook123@users.noreply.github.com>
2023-11-26 18:45:31 -0500

    Update PauseGUI.py

--- PauseGUI.py
+++ PauseGUI.py
@@ -1,26 +1,29 @@
+# -*- coding: utf-8 -*-
 import pygame
 
 class Pause(object):
-    def __init__(self,screen,font,status):
-        self.status=False
-        self.font=font
-        self.screen=screen
-        self.clicked_pause=False
-        self.clicked_resume=False
-        self.clicked_quit=False
+    def __init__(self, screen:list, font:str, status:bool):
+        self.status = False
+        self.font = font
+        self.screen = screen
+        self.clicked_pause = False
+        self.clicked_resume = False
+        self.clicked_quit = False
         self.clicked = False
-        self.Mainfont=pygame.font.Font('freesansbold.ttf', 40)
-    def show(self):
-        self.clicked_resume=False
+        self.Mainfont = pygame.font.Font('freesansbold.ttf', 40)
+    def show(self) -> bool:
+        ''' This function displays the pause button on screen. '''
+        self.clicked_resume = False
         pos = pygame.mouse.get_pos()
-        p=pygame.draw.rect(self.screen, 'black', [50, 50, 20, 20], 0, 10)
+        p = pygame.draw.rect(self.screen, 'black', [50, 50, 20, 20], 0, 10)
         pause_text = self.font.render('II', True, 'white')
         self.screen.blit(pause_text, (50, 50))
         if p.collidepoint(pos):
             if pygame.mouse.get_pressed()[0] == 1:
-                self.clicked_pause=True
+                self.clicked_pause = True
         return self.clicked_pause
-    def PauseMenu(self):
+    def PauseMenu(self) -> bool:
+        ''' This function draws the pause menu with the resume button on screen. '''
         pos = pygame.mouse.get_pos()
         pygame.draw.rect(self.screen, 'white', [50, 200, 800, 300],0, 10)
         pygame.draw.rect(self.screen, 'dark grey', [70, 220, 760, 260], 0, 10)
@@ -33,10 +36,11 @@ class Pause(object):
         pauseMenu_text = self.Mainfont.render(f'Pause Menu', True, 'white')
         if resume.collidepoint(pos):
             if pygame.mouse.get_pressed()[0] == 1:
-                self.clicked_resume=True
-                self.clicked_pause=False
+                self.clicked_resume = True
+                self.clicked_pause = False
         return self.clicked_resume
-    def quit_button(self):
+    def quit_button(self) -> bool:
+        ''' This function draws the quit button onto the pause menu. '''
         pos = pygame.mouse.get_pos()
         quits = pygame.draw.rect(self.screen, 'Blue', [470, 335, 150, 50], 0, 10)
         quit_text = self.font.render(f'Quit', True, 'white')
@@ -47,7 +51,9 @@ class Pause(object):
                 self.clicked_quit=True
                 self.clicked_pause=False
         return self.clicked_quit
-    def start_over(self):
-        self.clicked_quit=False
-    def setStatus(self, status):
+    def start_over(self) -> None:
+        ''' This function sets the value of the clicked_quit variable. '''
+        self.clicked_quit = False
+    def setStatus(self, status) -> None:
+        ''' This function sets the value of the status variable. '''
         self.status = status
