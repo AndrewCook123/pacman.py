3dc929594beede3da1e1ca93764033ebc10b1983
AndrewCook123 <148015806+AndrewCook123@users.noreply.github.com>
2023-11-26 18:46:44 -0500

    Update victoryGUI.py

--- victoryGUI.py
+++ victoryGUI.py
@@ -1,29 +1,29 @@
 import pygame
-from pauseGUI import pause
 import saveGUI
 
 class VictoryGUI(object):
-    def __init__(self, screen,font,level,lives,date,score):
-        self.lives=lives
-        self.level=level
-        self.date=date
-        self.score=score
-        self.clicked_next=False
-        self.screen=screen
-        self.font=font
-        self.timer=0
-        self.check=False
-        self.clicked=False
-        self.new=saveGUI.saveGUI(self.screen, self.font,self.level,self.lives,self.date,self.score)
+    def __init__(self, screen:list, font:str, level:list, lives:int, date:str, score:int):
+        self.lives = lives
+        self.level = level
+        self.date = date
+        self.score = score
+        self.clicked_next = False
+        self.screen = screen
+        self.font = font
+        self.timer = 0
+        self.check = False
+        self.clicked = False
+        self.new = saveGUI.saveGUI(self.screen, self.font,self.level,self.lives,self.date,self.score)
         
-    def victory(self):
+    def victory(self) -> None:
+        ''' This function draws the victory menu and next level button on screen. '''
         self.new.setlevel(self.level)
         self.new.setlives(self.lives)
         self.new.setscore(self.score)
         pos = pygame.mouse.get_pos()
         pygame.draw.rect(self.screen, 'white', [50, 200, 800, 300],0, 10)
         pygame.draw.rect(self.screen, 'dark gray', [70, 220, 760, 260], 0, 10)
-        top=pygame.draw.rect(self.screen, 'blue', [350, 350, 150, 50], 0, 10)
+        top = pygame.draw.rect(self.screen, 'blue', [350, 350, 150, 50], 0, 10)
         start_text = self.font.render(f'Next Level->', True, 'white')
         self.screen.blit(start_text, (365, 370))
         if top.collidepoint(pos):
@@ -45,11 +45,14 @@ class VictoryGUI(object):
         if pygame.mouse.get_pressed()[0] == 0:
             pass
         return self.clicked
-    def setlevel(self,level):
+    def setlevel(self,level) -> None:
+        ''' This function sets the value of the level variable. '''
         self.level = level
-    def setlives(self,lives):
+    def setlives(self,lives) -> None:
+        ''' This function sets the value of the lives variable. '''
         self.lives = lives
-    def setscore(self,score):
+    def setscore(self,score) -> None:
+        ''' This function sets the value of the score variable. '''
         self.score = score
     
     
