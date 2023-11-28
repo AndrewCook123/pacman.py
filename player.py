08cf6c1500e10a2adfbb13aef92eb909aad4a259
cdragun53 <143451754+cdragun53@users.noreply.github.com>
2023-11-28 17:13:33 -0500

    Update player.py

--- player.py
+++ player.py
@@ -1,26 +1,18 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import pygame
+"""
+Created on Tue Oct 24 09:08:16 2023
+
+@author: corydragun
+"""
+import pygame 
+
 
 class Player(object):
     def __init__(self):
-        self.WIDTH = 900
-        self.HEIGHT = 950
-    
-    def move_player(self, play_x:str, play_y:str, turns_allowed:list, direction:int, player_speed:int) -> int | int:
-        ''' This function is responsible for moving the player. '''
-        # right = 0, left = 1, up = 2, down = 3
-        if direction == 0 and turns_allowed[0]:
-            play_x += player_speed
-        elif direction == 1 and turns_allowed[1]:
-            play_x -= player_speed
-        if direction == 2 and turns_allowed[2]:
-            play_y -= player_speed
-        elif direction == 3 and turns_allowed[3]:
-            play_y += player_speed
-        return play_x, play_y
-    
-    def draw_player(self, direction:int, player_x:int, player_y:int, counter:int, screen:list, player_images:str) -> None:
-        ''' This function draw the player on screen. '''
+        self.HEIGHT=950
+        self.WIDTH=900
+    def draw_player(self, direction, player_x, player_y, counter,screen,player_images):
         # 0-RIGHT, 1-LEFT, 2-UP, 3-DOWN
         if direction == 0:
             screen.blit(player_images[counter // 5], (player_x, player_y))
@@ -31,8 +23,7 @@ class Player(object):
         elif direction == 3:
             screen.blit(pygame.transform.rotate(player_images[counter // 5], 270), (player_x, player_y))
                 
-    def check_position(self, centerx:int, centery:int, direction:int, level:list) -> list:
-        ''' This function checks the players location on the board. '''
+    def check_position(self, centerx, centery, direction, level):
         turns = [False, False, False, False]
         num1 = (self.HEIGHT - 50) // 32
         num2 = (self.WIDTH // 30)
@@ -76,17 +67,29 @@ class Player(object):
                         turns[0] = True
             if direction == 0 or direction == 1:
                 if 12 <= centerx % num2 <= 18:
-                    if level[(centery + num1) // num1][centerx // num2] == 10:
+                    if level[(centery + num1) // num1][centerx // num2] ==10:
                             turns[3] = True
-                    if level[(centery - num1) // num1][centerx // num2] == 10:
+                    if level[(centery - num1) // num1][centerx // num2] ==10:
                         turns[2] = True
                 if 12 <= centery % num1 <= 18:
-                    if level[centery // num1][(centerx - num3) // num2] == 10:
+                    if level[centery // num1][(centerx - num3) // num2] ==10:
                         turns[1] = True
-                    if level[centery // num1][(centerx + num3) // num2] == 10:
+                    if level[centery // num1][(centerx + num3) // num2] ==10:
                         turns[0] = True
         else:
             turns[0] = True
             turns[1] = True
 
         return turns
+        
+    def move_player(self, play_x, play_y, turns_allowed, direction,player_speed):
+        # r, l, u, d
+        if direction == 0 and turns_allowed[0]:
+            play_x += player_speed
+        elif direction == 1 and turns_allowed[1]:
+            play_x -= player_speed
+        if direction == 2 and turns_allowed[2]:
+            play_y -= player_speed
+        elif direction == 3 and turns_allowed[3]:
+            play_y += player_speed
+        return play_x, play_y
