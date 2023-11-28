466a6683f20037dfde4fa08fb21e990d41aa6478
cdragun53 <143451754+cdragun53@users.noreply.github.com>
2023-11-28 17:12:49 -0500

    Update pacman.py

--- pacman.py
+++ pacman.py
@@ -1,18 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 28 16:31:49 2023
+
+@author: corydragun
+"""
+
+# Build Pac-Man from Scratch in Python with PyGame!!
+
 # Build Pac-Man from Scratch in Python with PyGame!!
 from level import Level
 import pygame
 
 
-
-
-
-
-
-
-
-
-
-
-
 pygame.init()
-Level().gameLoop()
+Level().game_loop()
+
